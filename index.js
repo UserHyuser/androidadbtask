c83733a35f8d0e76e4675ce0e62549da5c0a7e69
UserHyuser <drachevskii@yandex.ru>
2019-12-01 22:37:17 +0700

    v1.0

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// let Promise = require('bluebird')
 const {exec} = require('child_process');
 const adb = require('adbkit');
 const sharp = require('sharp');
@@ -97,7 +96,7 @@ async function compareScreenshots(path, action) {
 					// console.log('Screenshots are equal!');
 					resolve(equal);
 				} else {
-					console.log(`Screenshots ar'nt equal. May be you did something wrong? Or just emulator is too fckn slow????? Trying again...`);
+					console.log(`Screenshots aren't equal. May be you did something wrong? Or just emulator is too fckn slow????? Trying again...`);
 					await wait(3000);
 					resolve(false)
 				}
@@ -108,7 +107,7 @@ async function compareScreenshots(path, action) {
 	})
 }
 
-async function verifyScreenshots(action) {
+async function verifyScreenshots(action, lastStep) {
 	let path = './screenshots/';
 	return new Promise(async (resolve, reject) => {
 			let res = false;
@@ -118,6 +117,10 @@ async function verifyScreenshots(action) {
 						await wait(100);
 						await cropPhoto(path, action + '.png').then(async value => {
 							res = await compareScreenshots(path, action);
+							if(lastStep && !res){
+								res = true;
+								reject('Do not click. Just collapse');
+							}
 						});
 					}));
 			}
@@ -126,6 +129,7 @@ async function verifyScreenshots(action) {
 	)
 }
 
+/* Main process */
 client.listDevicesWithPaths(async function (err, devices) {
 	try {
 		await client.shell(correctPath, 'input')
@@ -133,13 +137,14 @@ client.listDevicesWithPaths(async function (err, devices) {
 		console.log('Device is not connected');
 		process.exit(0);
 	}
-	await devices.forEach(await function (item) {
+	await devices.forEach(await async function (item) {
 		arrDevices.push(item);
 		console.log(item);
 		if (item.id === correctPath) {
 			emulatorDevice = item;
 			if (emulatorDevice.type === 'offline') {
-				console.log('Device is offline');
+				await exec('adb connect 127.0.0.1:62001');
+				console.log('Device is offline. Restart script');
 				process.exit(0);
 			}
 		}
@@ -154,14 +159,14 @@ client.listDevicesWithPaths(async function (err, devices) {
 		await verifyScreenshots('GoogleAppStart')
 		console.log('Asking for Elon')
 	});
-	await wait(1500);
+	await wait(2500);
 	await (async () => {
 		return new Promise(async (resolve, reject) => {
 			await client.shell(correctPath, 'input text "Elon%sMusk"', async () => {
-				await wait(2000);
+				await wait(4000);
 				await verifyScreenshots('GoogleApp').then(async value => {
 					await client.shell(correctPath, 'input keyevent 66');
-					console.log('Ask Google for Elon Musk')
+					console.log('Asked Google for Elon Musk')
 					resolve('Asked')
 				})
 			});
@@ -172,9 +177,9 @@ client.listDevicesWithPaths(async function (err, devices) {
 		return new Promise(async (resolve, reject) => {
 			await verifyScreenshots('GoogleIsReady').then(async value => {
 				await client.shell(correctPath, 'input keyevent 93');
-				await wait(700);
+				await wait(1000);
 				await client.shell(correctPath, 'input keyevent 93');
-				await wait(700);
+				await wait(1000);
 				await client.shell(correctPath, 'input keyevent 93');
 				console.log('Scrolled')
 				resolve('Ready to click');
@@ -184,15 +189,23 @@ client.listDevicesWithPaths(async function (err, devices) {
 	await wait(3000);
 	await (async () => {
 		return new Promise(async (resolve, reject) => {
-			await verifyScreenshots('LoadingPage').then(async value => {
-				await client.shell(correctPath, 'input tap 300 560', async function (err, output) {
-					console.log('Clicked');
-					await wait(6000);
+			await wait(3000);
+			await verifyScreenshots('LoadingPage', true)
+				.then(async value => {
+					await client.shell(correctPath, 'input tap 400 285', async function (err, output) {
+						console.log('Clicked');
+						await wait(8000);
+						await client.shell(correctPath, 'input keyevent 3', async function (err, output) {
+							console.log('Done');
+						});
+					});
+				})
+				.catch(async value =>{
+					console.log(`Page won't open 'cause screenshots are different`);
 					await client.shell(correctPath, 'input keyevent 3', async function (err, output) {
 						console.log('Done');
 					});
-				});
-			})
+				})
 		})
 	})();
 });
